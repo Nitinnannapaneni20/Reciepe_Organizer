c6f17026f58cec46e0686d5f8dcaa4ce9f3a987e
nitin <nitinnannapaneni@gmail.com>
2024-10-25 20:43:19 +0100

    Styled the index page a bit better and also added in edit and delete

--- app/Http/Controllers/RecipeController.php
+++ app/Http/Controllers/RecipeController.php
@@ -12,4 +12,29 @@ class RecipeController extends Controller
         $recipes = Recipe::all();
         return view('recipes.index', compact('recipes'));
     }
+
+    // Show the edit form
+    public function edit($id)
+    {
+        $recipe = Recipe::findOrFail($id);
+        return view('recipes.edit', compact('recipe'));
+    }
+
+    // Update the recipe
+    public function update(Request $request, $id)
+    {
+        $recipe = Recipe::findOrFail($id);
+        $recipe->update($request->all());
+
+        return redirect()->route('recipes.index')->with('success', 'Recipe updated successfully.');
+    }
+
+    // Delete the recipe
+    public function destroy($id)
+    {
+        $recipe = Recipe::findOrFail($id);
+        $recipe->delete();
+
+        return redirect()->route('recipes.index')->with('success', 'Recipe deleted successfully.');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,13 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RecipeController;
 
-Route::get('/', [RecipeController::class, 'index']);
+Route::get('/', [RecipeController::class, 'index'])->name('recipes.index');
 
+// Route to show edit form
+Route::get('/recipes/{id}/edit', [RecipeController::class, 'edit'])->name('recipes.edit');
+
+// Route to update a recipe
+Route::put('/recipes/{id}', [RecipeController::class, 'update'])->name('recipes.update');
+
+// Route to delete a recipe
+Route::delete('/recipes/{id}', [RecipeController::class, 'destroy'])->name('recipes.destroy');
